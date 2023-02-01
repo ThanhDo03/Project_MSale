c12cd929943b45f855d03857a9b0849f1516117c
ThanhDo03 <tranthanhdo2407@gmail.com>
2023-02-01 15:55:42 +0700

    first commit

---
+++ resources/views/layout/index.blade.php
@@ -0,0 +1,9 @@
+@INCLUDE('layout.head')
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+        @INCLUDE('layout.navbar')
+        @INCLUDE('layout.sidebar')
+        @INCLUDE('layout.footer')
+    </div>
+    <!-- End of Page Wrapper -->
+@INCLUDE('layout.js')
---
+++ resources/views/layout/js.blade.php
@@ -0,0 +1,10 @@
+<script src="{{asset('js/jquery.min.js')}" crossorigin="anonymous"></script>
+<script src="{{asset('js/bootstrap.bundle.min.js')}" crossorigin="anonymous"></script>
+<script src="{{asset('js/scripts.js')}"></script>
+<script src="{{asset('js/Chart.min.js')}" crossorigin="anonymous"></script>
+
+<script src="{{asset('js/chart-area-demo.js')}"></script>
+<script src="{{asset('js/chart-bar-demo.js')}"></script>
+<script src="{{asset('js/jquery.dataTables.min.js')}" crossorigin="anonymous"></script>
+<script src="{{asset('js/dataTables.bootstrap4.min.js')}" crossorigin="anonymous"></script>
+<script src="{{asset('js/datatables-demo.js')}"></script>
